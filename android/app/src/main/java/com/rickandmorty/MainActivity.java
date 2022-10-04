24f32fcd720188739792d5f9b19f264b6622925c
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2022-10-03 22:59:02 -0300

    Final fixes

--- android/app/src/main/java/com/rickandmorty/MainActivity.java
+++ android/app/src/main/java/com/rickandmorty/MainActivity.java
@@ -2,6 +2,7 @@ package com.rickandmorty;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
@@ -24,6 +25,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 
   public static class MainActivityDelegate extends ReactActivityDelegate {
